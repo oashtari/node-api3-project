e44505bd54f5318dd264141dbf1d27abddc0fae5
oashtari <oashtari@gmail.com>
2020-03-18 19:36:58 -0700

    hoping adding heroku does not break my project

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // code away!
+require('dotenv').config();
 const server = require('./server.js');
 
-server.listen(7000, () => {
-    console.log('\n* Server Running on http://localhost:7000 *\n');
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
